ea3b7f2e17f9481dedb4a996d44361fa3fa06454
jinjiacheng2018 <1935922807@qq.com>
2018-12-26 11:57:31 +0800

    测试JDK8去除集合重复数据

--- 04-spring-boot-druid/src/test/java/com/tgram/sboot/ApplicationTests.java
+++ 04-spring-boot-druid/src/test/java/com/tgram/sboot/ApplicationTests.java
@@ -1,6 +1,7 @@
 package com.tgram.sboot;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.tgram.sboot.util.MyDateUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,11 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -57,8 +63,62 @@ public class ApplicationTests
         System.out.println(environment.getProperty("spring.datasource.driver-class-name"));
     }
 
+    /**
+     * 测试配置类配置数据源
+     */
     @Test
     public void test2(){
         System.err.println(druidDataSource);
+    }
+
+    /**
+     * 测试日期工具类
+     */
+    @Test
+    public void test3(){
+        String dateStr = "2018-12-26";
+        Date date = MyDateUtil.parseDate(dateStr, "yyyy-MM-dd");
+        System.out.println(date);
+
+        Date startDate = MyDateUtil.incr(date, -30);
+        Date endDate = MyDateUtil.incr(date, 30);
+
+        System.out.println("----> " + MyDateUtil.dateFormate(startDate,"yyyy-MM-dd HH:mm:ss"));
+        System.out.println("****> " + MyDateUtil.dateFormate(endDate,"yyyy-MM-dd HH:mm:ss"));
+    }
+
+    /**
+     * 测试日期相关
+     */
+    @Test
+    public void testDate(){
+        Date date = new Date();
+        System.out.println(MyDateUtil.dateFormate(date,"yyyy-MM-dd"));
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.DAY_OF_MONTH,-30);
+        System.out.println(calendar.getTime() + " <-> " + MyDateUtil.dateFormate(calendar.getTime(),"yyyy-MM-dd"));
+
+        calendar.setTime(date);
+        calendar.add(Calendar.DAY_OF_MONTH,30);
+        System.out.println(calendar.getTime() + " <*> " + MyDateUtil.dateFormate(calendar.getTime(),"yyyy-MM-dd"));
+    }
+
+    /**
+     * 测试JDK8去除List集合中重复的元素
+     */
+    @Test
+    public void testJdk8List(){
+        List<String> list = Arrays.asList("helloworld","java","oracle","c","c#","oracle");
+        list.forEach(str -> System.out.print(str + "  "));
+
+        System.out.println();
+
+        // 去除重复之后需要使用一个新的集合集合
+        // List<String> list2 = list.stream().distinct().collect(Collectors.toList());
+        // list2.forEach(str2 -> System.out.print(str2 + "  "));
+
+        list.stream().distinct().collect(Collectors.toList()).forEach(str3 -> System.out.print(str3 + "  "));
     }
 }
