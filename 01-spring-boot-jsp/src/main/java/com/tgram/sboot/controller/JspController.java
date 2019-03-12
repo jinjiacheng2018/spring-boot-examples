1fc2a2f0175cb1de1bc291a556b0b6781a008847
jinjiacheng2018 <1935922807@qq.com>
2019-03-12 10:59:47 +0800

    Integer，Boolean判断对象

--- 01-spring-boot-jsp/src/main/java/com/tgram/sboot/controller/JspController.java
+++ 01-spring-boot-jsp/src/main/java/com/tgram/sboot/controller/JspController.java
@@ -1,5 +1,6 @@
 package com.tgram.sboot.controller;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +20,19 @@ public class JspController {
     public String index(Model model){
         model.addAttribute("msg","Hello,SpringBoot-JSP");
         return "success";
+    }
+
+    public static void main(String[] args) {
+        Boolean i1 = true;
+        Boolean i2 = true;
+        System.out.println(i1 == i2);
+        System.out.println(i1.equals(i2));
+
+        System.out.println("--------------");
+
+        Integer i3 = 100;   // i3=128
+        Integer i4 = 100;   // i4=128
+        System.out.println(i3 == i4);
+        System.out.println(i3.equals(i4));
     }
 }
