8406866b1f4dd7f76718a4a0dfcaa71a5ce40f6c
jinjiacheng2018 <1935922807@qq.com>
2019-03-22 11:46:33 +0800

    SpringBoot启动类测试自动注入的Bean的个数

--- 02-spring-boot-mybatis/src/main/java/com/tgram/sboot/SpringBootMybatisApplication.java
+++ 02-spring-boot-mybatis/src/main/java/com/tgram/sboot/SpringBootMybatisApplication.java
@@ -1,7 +1,12 @@
 package com.tgram.sboot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 /**
  *<p> Description: 描述信息 </p>
@@ -17,4 +22,23 @@ public class SpringBootMybatisApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootMybatisApplication.class,args);
     }
+
+    /**
+     * 查看SpringBoot自动注入的那些Bean对象
+     * @param ctx ApplicationContextTomcat上下文对象
+     * @return CommandLineRunner
+     */
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (int i = 0; i < beanNames.length; i++) {
+                System.out.println(i + "------>:" + beanNames[i]);
+            }
+        };
+    }
+
 }
+
